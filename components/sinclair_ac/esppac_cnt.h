787d6a18d8c60b46dad36e3d8680af70781cd6fa
Patress <42972161+x15pa3ck15x@users.noreply.github.com>
2024-03-10 14:43:37 +0100

    Update esppac_cnt.h

--- components/sinclair_ac/esppac_cnt.h
+++ components/sinclair_ac/esppac_cnt.h
@@ -136,7 +136,7 @@ namespace protocol {
     static const uint8_t SET_CONST_BIT_MASK    = 0b00000010;
 
     /* time constraints */
-    static const unsigned long TIME_REFRESH_PERIOD_MS   =  5000;
+    static const unsigned long TIME_REFRESH_PERIOD_MS   =  300;
     static const unsigned long TIME_TIMEOUT_INACTIVE_MS = 1000;
 }
 
